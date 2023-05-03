8dbf4164dafa32d9430aa7f236f19c2892649c2c
Massimo Fasciano <massimo@mafalitic.com>
2023-05-03 15:36:00 -0400

    dist

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
+.PHONY : build server dist
+
 build:
 	wasm-pack build --target web
 
 server:
 	python -m http.server
 
+dist:
+	mkdir -p dist/
+	cp index.html dist/
+	mkdir -p dist/pkg
+	cp pkg/*.wasm pkg/*.js dist/pkg/
+	cd dist; zip -9r ai_wargame_web.zip index.html pkg
+	rm -rf dist/index.html dist/pkg
+	
--- src/lib.rs
+++ src/lib.rs
@@ -10,35 +10,20 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 extern "C" {
     #[wasm_bindgen(js_namespace = console, js_name = log)]
     fn console_log(s: &str);
-    #[wasm_bindgen(js_namespace = document, js_name = write)]
-    fn document_write(s: &str);
-    #[wasm_bindgen(js_name = update_board)]
-    fn update_board_js(s: &str);
 }
 
 macro_rules! console_log {
     ($($t:tt)*) => (console_log(&format_args!($($t)*).to_string()))
 }
 
-fn update_board(s: &str) {
-    // let window = web_sys::window().expect("no global `window` exists");
-    // let document = window.document().expect("should have a document on window");
-    // let element = document.get_element_by_id("board").expect("should find board");
-    // element.set_text_content(Some(s));
-    
-    // update_board_js(s);
-}
-
 #[wasm_bindgen]
 pub fn web_ai_wargame() {
     std::panic::set_hook(Box::new(console_error_panic_hook::hook));
 
-    update_board("init from rust");
-
     let mut options = GameOptions::default();
     options.max_depth = Some(6);
     options.max_moves = Some(150);
-    // options.max_seconds = Some(5.0);
+    options.max_seconds = Some(5.0);
     {
         use heuristics::*;
         let _h1 = units_health_weights_bias(10,10,100) * 10
@@ -84,5 +69,4 @@ pub fn pretty_print(game: &Game) {
     buffer.flush().unwrap();
     let board = String::from_utf8_lossy(&buffer);
     console_log!("{}",board);
-    update_board(board.as_ref());
 }
